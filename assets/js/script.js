8caca9af9e5cf8a2c8f3848872a6813ca3ea1f10
rabiiiii18 <np03cs4s220260@heraldcollege.edu.np>
2024-12-17 20:43:41 +0545

    update weather forecast

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,10 +10,11 @@ const tempText = document.querySelector(".temp-text");
 const humidityText = document.querySelector(".humidity-txt");
 const windSpeedTxt = document.querySelector(".windSpeed-txt");
 const weatherTxt = document.querySelector(".weather-txt");
-const smallWeatherTxt=document.querySelector(".smallWeatherTxt");
+const smallWeatherTxt = document.querySelector(".smallWeatherTxt");
 const dateTxt = document.querySelector(".currentDate-text");
 const imgDesc = document.querySelector(".imgDescription");
-const time=document.querySelector(".CurrentTime");
+
+const forecastDay = document.querySelector(".dayForecast");
 
 const apiKey = "60e036ffca9ddd83be2aa9eef15661a3";
 
@@ -33,20 +34,20 @@ cityInput.addEventListener('keydown', (event) => {
     }
 })
 
-async function getFetchData(city) {
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`
+async function getFetchData(endPoint, city) {
+    const url = `https://api.openweathermap.org/data/2.5/${endPoint}?q=${city}&appid=${apiKey}&units=metric`
     const response = await fetch(url);
     return response.json()
 }
 async function updateWeather(city) {
-    const weatherData = await getFetchData(city);
+    const weatherData = await getFetchData('weather', city);
     if (weatherData.cod == 404) {
         showSection(notFoundSection);
         return
     }
     console.log(weatherData);
     const { name: country,
-        main: { temp, humidity,pressure, },
+        main: { temp, humidity, pressure, },
         wind: { speed },
         weather: [{ id, main }]
 
@@ -57,13 +58,32 @@ async function updateWeather(city) {
     humidityText.innerHTML = Math.round(humidity) + "%";
     windSpeedTxt.innerHTML = Math.round(speed) + " mph";
     weatherTxt.textContent = main;
-    smallWeatherTxt.textContent=main;
+    smallWeatherTxt.textContent = main;
     dateTxt.textContent = getCurrentDate();
     imgDesc.src = `./assets/image/weather/${getWeatherIcon(id)}`
-    // time.innerHTML = getCurrentTime();
-    
+
+    await updateForeCastData(city);
 
     showSection(mainSection);
+}
+async function updateForeCastData(city) {
+    const ForeCastData = await getFetchData('forecast', city)
+    const time = "12:00:00"
+
+    const todayDate = new Date().toISOString().split("T")[0];
+
+    forecastDay.innerHTML = ''
+
+    ForeCastData.list.forEach(forecastWeather => {
+        if (forecastWeather.dt_txt.includes(time) && !forecastWeather.dt_txt.includes(todayDate)) {
+
+            updateForeCastItens(forecastWeather);
+            console.log(forecastWeather);
+        }
+
+    })
+    // console.log(ForeCastData);
+
 }
 
 
@@ -77,7 +97,32 @@ function getWeatherIcon(id) {
     if (id < 800) return 'clear.svg'
     else return 'clouds.svg'
 }
+function updateForeCastItens(weatherData) {
+    
+    const {
+        dt_txt: date,
+        weather : [{ id }],
+        main : { temp }
 
+    } = weatherData;
+    const forecastDate=new Date(date);
+    const dateFormat={
+        day:'2-digit',
+        month:'short'
+
+    }
+    const dateresult=forecastDate.toLocaleDateString('en-US',dateFormat)
+
+    const forecatsItem =
+        `<div class="card">
+            <h3>${dateresult}</h3>
+            <img src="./assets/image/weather/${getWeatherIcon(id)}" alt="">
+            <h3>${Math.round(temp)}°C</h3>
+         </div>`;
+
+    forecastDay.insertAdjacentHTML('beforeend', forecatsItem);
+
+}
 
 function getCurrentDate() {
     const date = new Date();
@@ -88,11 +133,6 @@ function getCurrentDate() {
     }
     return date.toLocaleDateString('en-GB', option);
 }
-
- 
-
-
-
 function showSection(section) {
     [mainSection, notFoundSection, searchCitySection].forEach(section =>
         section.style.display = 'none')
